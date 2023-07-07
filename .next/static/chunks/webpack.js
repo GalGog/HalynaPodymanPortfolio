6190733c51b204b72f1ab8d5f29870e8a276d839
GalGog <galynna00@gmail.com>
2023-07-07 10:15:49 +0100

    change demo page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7f2179e5a0610a42"; }
+/******/ 		__webpack_require__.h = function() { return "dfbe404ec4527905"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
